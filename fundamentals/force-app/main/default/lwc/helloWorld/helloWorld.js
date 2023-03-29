4d7af8e9af8147ac536fa5d8eced27a3c31b08f0
Darryl Le Roux <devdarryl@outlook.com>
2023-03-29 14:18:19 +0200

    Added getters

--- fundamentals/force-app/main/default/lwc/helloWorld/helloWorld.js
+++ fundamentals/force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,11 +1,40 @@
-import { LightningElement } from "lwc";
+import { LightningElement, track } from "lwc";
 
 export default class HelloWorld extends LightningElement {
   fullName = "Darryl Le Roux";
   title = "Developer";
 
-  // change title handler
+  // change title handler for the profession example
   handleTitleChange(event) {
     this.title = event.target.value;
+  }
+
+  // address object for @track example
+  // @track allows you to go into nested values
+  @track address = {
+    city: "Johannesburg",
+    postcode: 1929,
+    country: "South Africa"
+  };
+
+  // change the city handler for @track example
+  handleCityChange(event) {
+    this.address.city = event.target.value;
+    // example for not using @track
+    // this.address = {...this.address, "city": event.target.value}
+  }
+
+  // getter example
+  // allows the method to update dynamically
+  users = ["John", "Peter", "Paul"];
+  num1 = 10;
+  num2 = 20;
+
+  get firstUser() {
+    return this.users[0];
+  }
+
+  get multiply() {
+    return this.num1 * this.num2;
   }
 }
