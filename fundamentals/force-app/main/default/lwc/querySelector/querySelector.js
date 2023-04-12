32fcdf1250892aff3e822fbe511f422750d2d30c
Darryl Le Roux <devdarryl@outlook.com>
2023-04-12 15:20:58 +0200

    Added query Selector demo

---
+++ fundamentals/force-app/main/default/lwc/querySelector/querySelector.js
@@ -0,0 +1,25 @@
+import { LightningElement } from "lwc";
+
+export default class QuerySelector extends LightningElement {
+  userNames = ["John", "Jane", "Joe", "Jill"];
+
+  fetchDetailHandler() {
+    const elem = this.template.querySelector("h1");
+    elem.style.border = "1px solid red";
+    console.log(elem.innerText);
+
+    const userElements = this.template.querySelectorAll(".name");
+    // create a new array from the node list
+    Array.from(userElements).forEach((item) => {
+      // print the text of each element
+      console.log(item.innerText);
+      // set the title attribute of each element
+      item.setAttribute("title", item.innerText);
+    });
+
+    // lwc:dom="manual" is required for this to work
+    const childElem = this.template.querySelector(".child");
+    // set the innerHTML of the child element
+    childElem.innerHTML = `<p>Child Element</p>`;
+  }
+}
