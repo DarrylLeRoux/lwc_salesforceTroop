4154bc2de31c21468f0cbcb1c3f43c0227a1ffe5
Darryl Le Roux <devdarryl@outlook.com>
2023-04-13 10:56:21 +0200

    Added examples of lifecycle hooks and printing to the console

--- fundamentals/force-app/main/default/lwc/lifeCycleChild/lifeCycleChild.js
+++ fundamentals/force-app/main/default/lwc/lifeCycleChild/lifeCycleChild.js
@@ -1,31 +1,24 @@
 import { LightningElement } from "lwc";
 
-export default class LifeCycleParent extends LightningElement {
+export default class LifeCycleChild extends LightningElement {
   // fires when a component is added to the DOM
   // don't add attributes to the host element in the constructor
   constructor() {
     super();
-    console.log("Parent Constructor Called");
+    console.log("Child Constructor Called");
   }
 
   // use to perform any initialization
   // use to fetch data or listen for events
   // don't use to change state or update property of a component - use getters and setters
   connectedCallback() {
-    console.log("Parent Connected Callback Called");
+    console.log("Child Connected Callback Called");
   }
 
   // fires when a components rendering is done
   // do not use to change state or update property of a component
   // don't update a wire adapter configuration property
   renderedCallback() {
-    console.log("Parent Rendered Callback Called");
-  }
-
-  // initialize name property
-  name;
-  // this will call renderedCallback() method for each change
-  changeHandler(event) {
-    this.name = event.target.value;
+    console.log("Child Rendered Callback Called");
   }
 }
